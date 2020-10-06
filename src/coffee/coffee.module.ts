6d443bf2dbe7af43dd98c9ab440106c7ff9c03d8
Serhii Manchenko <grandx.go@gmail.com>
2020-10-06 23:02:00 +0300

    create flavor entity, refactor basic crud methods

--- src/coffee/coffee.module.ts
+++ src/coffee/coffee.module.ts
@@ -1,8 +1,12 @@
+import { Flavor } from './entities/flavor.entity';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { CoffeeController } from './coffee.controller';
 import { CoffeeService } from './coffee.service';
+import { Coffee } from './entities/coffee.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
     controllers: [CoffeeController],
     providers: [CoffeeService],
 })
